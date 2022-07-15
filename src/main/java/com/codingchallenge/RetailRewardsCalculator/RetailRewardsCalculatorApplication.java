e0866061f47251cdf9f6dbc5f7603955811565ce
Mounika <mounikaujava@gmail.com>
2022-07-15 19:09:16 +0530

    Added Spring boot project with basic dependencies

---
+++ src/main/java/com/codingchallenge/RetailRewardsCalculator/RetailRewardsCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.codingchallenge.RetailRewardsCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailRewardsCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailRewardsCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingchallenge/RetailRewardsCalculator/RetailRewardsCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingchallenge.RetailRewardsCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailRewardsCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
